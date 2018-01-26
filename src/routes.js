f4fbf244dc96d920863ad4eacdf084750e67c156
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2018-01-26 14:15:17 +0100

    Add ClassContainer, ClassItem plus css/sass files

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 
 import BatchesContainer from './batches/BatchesContainer'
-import ClassPage from './batches/ClassPage'
+// import ClassPage from './batches/ClassPage'
 import StudentPage from './batches/StudentPage'
 
 export default class Routes extends Component {
@@ -11,9 +11,11 @@ export default class Routes extends Component {
     return (
       <div>
         <Route exact path="/" component={BatchesContainer} />
-        <Route path="/batches/:batchId" component={ClassPage} />
+
         <Route path="/batches/:batchId/students/" component={StudentPage} />
       </div>
     )
   }
 }
+
+    // <Route path="/batches/:batchId" component={ClassPage} />
