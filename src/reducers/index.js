1001d19de93e6a3bcf0c90e84e34502af122608f
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2018-01-27 17:38:12 +0100

    Add students to reducer index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 // src/reducers/index.js
 import batches from './batches'
+import students from './students'
 import loading from './loading'
 import loadErrors from './loadErrors'
 
 export default {
   loading,
   loadErrors,
-  batches
+  batches,
+  students
 }
